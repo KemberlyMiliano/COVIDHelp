8fa20b75445b0ff9b2825e528439e974382efcf3
KemberlyMiliano <kemberlymiliano@gmail.com>
2020-04-04 15:29:34 -0400

    Prism Configuration

---
+++ COVIDHelp/COVIDHelp/COVIDHelp/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using COVIDHelp.ViewModels;
+using COVIDHelp.ViewModels.NeededViewModels;
+using COVIDHelp.Views;
+using COVIDHelp.Views.NeededViews;
+using Prism;
+using Prism.Ioc;
+using Prism.Unity;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace COVIDHelp
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+        }
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<LoginPage, LoginPageViewModel>();
+            containerRegistry.RegisterForNavigation<SignUpPage, SignUpPageViewModel>();
+            containerRegistry.RegisterForNavigation<LoginPage, LoginPageViewModel>();
+        }
+    }
+
+}
---
+++ COVIDHelp/COVIDHelp/COVIDHelp/Views/NeededViews/NeededHomePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace COVIDHelp.Views.NeededViews
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NeededHomePage : ContentView
+    {
+        public NeededHomePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
