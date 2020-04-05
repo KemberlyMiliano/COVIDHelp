698dd1886ea4b27fcb50eb12a1747a5a2823846c
RafaelFernandez <rafaelfernandez0512@gmail.com>
2020-04-05 07:49:47 -0500

    ApiGooglePlace+User endpoint

--- COVIDHelp/COVIDHelp/COVIDHelp/Services/ApiCovitServices.cs
+++ COVIDHelp/COVIDHelp/COVIDHelp/Services/ApiCovitServices.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿using COVIDHelp.Models;
+using Refit;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace COVIDHelp.Services
 {
-    public class ApiCovitServices:ApiCovitServices
+    public class ApiCovitServices : IApiCovitServices
     {
+        public async Task PostUser([Body] User user)
+        {
+            var getRequest = RestService.For<IApiCovitServices>(ConfigApi.UrlApi);
+            await getRequest.PostUser(user);
+        }
     }
 }
