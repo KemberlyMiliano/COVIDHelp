1f3205575455107301ba6979bd952f9f10e6b9b4
KemberlyMiliano <kemberlymiliano@gmail.com>
2020-04-07 14:11:23 -0400

    DoItForMePage Done

--- COVIDHelp/COVIDHelp/COVIDHelp/ViewModels/DoItForMePageViewModel.cs
+++ COVIDHelp/COVIDHelp/COVIDHelp/ViewModels/DoItForMePageViewModel.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace COVIDHelp.ViewModels
 {
@@ -15,18 +16,36 @@ namespace COVIDHelp.ViewModels
         public Request Need { get; set; } = new Request();
         public ObservableCollection<Request> Requests { get; set; } = new ObservableCollection<Request>();
         public DelegateCommand AddToGlobalRequests { get; set; }
+        public DelegateCommand<Request> OnDeleteCommand { get; set; }
         public DelegateCommand AddToList { get; set; }
         public DoItForMePageViewModel(INavigationService navigationService, IPageDialogService dialogService, IApiCovitServices apiCovitServices) : base(navigationService, dialogService, apiCovitServices)
         {
             AddToGlobalRequests = new DelegateCommand(async () =>
             {
-                //anadir a la lista global de requerimientos para voluntarios que no sean profesionales de la salud
+                await DisplayAction();
             });
 
             AddToList = new DelegateCommand(() =>
             {
                 Requests.Add(Need);
             });
+
+            OnDeleteCommand = new DelegateCommand<Request>((param) =>
+            {
+                Requests.Remove(param);
+            });
+
+        }
+        public async Task DisplayAction()
+        {
+            bool action = await dialogService.DisplayAlertAsync("", "Estás seguro/a?", "Aceptar", "Cancelar");
+
+            if (action)
+            {
+                //anadir a la lista global de requerimientos para voluntarios que no sean profesionales de la salud
+                await navigationService.GoBackToRootAsync();
+            }
+
 
         }
     }
