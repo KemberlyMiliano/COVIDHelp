a2d9d86e3fa4671672571f95264e1177db694358
RafaelFernandez <rafaelfernandez0512@gmail.com>
2020-04-06 19:31:11 -0500

    need help

--- COVIDHelp/COVIDHelp/COVIDHelp/Models/NeedHelp.cs
+++ COVIDHelp/COVIDHelp/COVIDHelp/Models/NeedHelp.cs
@@ -4,7 +4,22 @@ using System.Text;
 
 namespace COVIDHelp.Models
 {
-    class NeedHelp
+    public class NeedHelp
     {
+        public string Nombre { get; set; }
+        public int Telefono { get; set; }
+        public int Dirrecion { get; set; }
+        public string Posicion { get; set; }
+        public string Email { get; set; }
+        public Int64 Cedula { get; set; }
+        public string DescripcionProblema { get; set; }
+    
+        public string NombreVoluntario { get; set; }
+        public int TelefonoVoluntario { get; set; }
+        public string EmailVoluntario { get; set; }
+        public string PosicionVoluntario { get; set; }
+        public Int64 CedulaVoluntario { get; set; }
+        public bool IsActiveHelp { get; set; }
+        public DateTime FechaEnviado { get; set; }
     }
 }
